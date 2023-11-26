68e9fa664e55fef5a9c4aa019f64eddd91b996f6
shesha4572 <shesha4572@gmail.com>
2023-11-26 11:50:51 +0530

    implement all functions

--- src/index.test.js
+++ src/index.test.js
@@ -16,5 +16,11 @@ test("Should subtract two numbers", () => {
 });
 
 // Test Case 3 (Multiplication)
+test("Should multiply two numbers", () => {
+  expect(multiply(25, 10)).toBe(250);
+});
 
 // Test Case 4 (Division)
+test("Should divide two numbers", () => {
+  expect(divide(20, 10)).toBe(2);
+});
